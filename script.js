9c8165d9193818abe673c1bf9d37b00084d32ca3
Arshamdnt <arsham6ta@gmail.com>
2023-03-23 21:12:37 +0430

    Question2

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 var Question1 = prompt("2 + 2 = ?")
 var Question2 = prompt("5 + 7 = ?")
-// var Question3 = prompt("3 + 9 = ?")
-// var Question4 = prompt("6 + 13 = ?")
+var Question3 = prompt("3 + 9 = ?")
+var Question4 = prompt("6 + 13 = ?")
 var Points = 0
 if (Question1 == 4) {
-	Points +1
+	Points +=1
 } else {
 	
 }
 if (Question2 == 12) {
-Points =+1	
+Points +=1	
 } else {
 }
-console.log(Points)
+if (Question3 == 12) {
+	Points += 1
+} else {
+	
+}
+if (Question4) {
+	Points+=1
+} else {
+	
+}
+alert(Points)
