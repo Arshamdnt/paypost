ccec743e8f33798c927f98fa2bc7adc03a5b0b27
Arshamdnt <arsham6ta@gmail.com>
2023-03-24 02:11:38 +0430

    forgot password

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 var users = [
-	{id : 1 , name : "arsham" , age :18},
-	{id : 2 , name : "behzad" , age :25},
-	{id : 3 , name : "dorsa" , age :19},
-	{id : 4 , name : "zeynab" , age :20},
+	{id : 1 , name : "arsham" , password : 000000023},
+	{id : 2 , name : "amin" , password : 1234},
+	{id : 3 , name : "sheyda" , password : 5678},
+	{id : 4 , name : "maryam" , password : 13831383},
 ]
-var finduser = users.find(function(users){
-return users.name === "arsham"
+var forgotpass = prompt("enter your username")
+var result = users.find(function(user){
+	return user.name === forgotpass
 })
-console.log(finduser)
+console.log(result)
