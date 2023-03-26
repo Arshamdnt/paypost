ebf9e76ec6218e955cbf3390a718218d34e5e28a
Arshamdnt <arsham6ta@gmail.com>
2023-03-26 23:40:46 +0430

    calculator

--- script.js
+++ script.js
@@ -1,12 +1,42 @@
-var minutes = prompt("Enter your time (minute): ", 0) // 60 => 1H
+var num1 = +prompt('Enter The First Number: ')
+var num2 = +prompt('Enter The Second Number: ')
 
+var userOperator = prompt('Enter The Operator: \n 1. + \n 2. - \n 3. * \n 4. / \n 5. **')
 
-// 1h = 60m | 120 => 2H | 180 => 3H
-
-var minutesToHours = minutes / 60
-
-if (isNaN(minutesToHours)) {
-	alert("عدد وارد شده معتبر نمی باشد")
+if (userOperator === '1') {
+    sum(num1, num2)
+} else if (userOperator === '2') {
+    tafrig(num1, num2)
+}
+else if (userOperator === '3') {
+    zarb(num1, num2)
+}
+else if (userOperator === '4') {
+    devide(num1, num2)
+}
+else if (userOperator === '5') {
+    power(num1, num2)
 } else {
-	alert(minutesToHours + " Hours")
+    alert('عملیات وارد شده معتبر نمی باشد')
+}
+
+function sum(num1, num2) {
+    // return num1 + num2\
+    alert(num1 + num2)
+}
+function tafrig(num1, num2) {
+    // return num1 - num2\
+    alert(num1 - num2)
+}
+function devide(num1, num2) {
+    // return num1 / num2\
+    alert(num1 / num2)
+}
+function zarb(num1, num2) {
+    // return num1 * num2\
+    alert(num1 * num2)
+}
+function power(num1, num2) {
+    // return num1 ** num2\
+    alert(num1 ** num2)
 }
